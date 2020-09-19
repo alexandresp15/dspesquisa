498f59409108291ec25f59e5504c2d31f4d549ac
Alexandre Pereira <alexandresp15@gmail.com>
2020-09-19 08:36:49 -0300

    Configuring front-web for production.

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,8 +20,8 @@ const initialPieData = {
     labels: [],
     series: []
 }
-
-const BASE_URL = 'http://localhost:8080'
+  
+const BASE_URL = 'https://sds1-alexandre.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
